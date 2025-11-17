1701523fa9d70d518fb18e02491e42de20d2ef95
Layci <spotamin@naver.com>
2025-11-17 21:47:47 +0900

    메인씬 작업 6,  메인 씬 캐릭터 공격 제작 3, 근접 이동 보정(근처 적 돌진 공격) 제작

---
+++ Assets/PROJECT 1/Scripts/Main Scene Control/MainPlayerAnimationManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProJect1
+{
+    public class MainPlayerAnimationManager : MonoBehaviour
+    {
+        public void PlayerAttack()
+        {
+            Debug.Log("메인 플레이어 공격!");
+        }
+    }
+}
